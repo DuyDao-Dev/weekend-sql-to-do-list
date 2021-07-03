316a15b22f0e572b96b2c8f8bb228765772b12a7
Duy Dao <jimmydao@gmail.com>
2021-07-02 23:00:54 -0500

    Trying to use bootstrap and trying to implement a modal for the first time. Modal not working

---
+++ server/public/scripts/client.js
@@ -0,0 +1,18 @@
+console.log('Client.js is ready');
+
+// $(document).ready(onReady);
+
+// function onReady(){
+//   //add listeners
+//   $('#addTask').on('click', popUpInputWindow);
+//   $('.submitButton').on('click', )
+// };
+
+// function popUpInputWindow (){
+//     $('.bg-modal').style.display = 'flex';
+// };
+
+// document.getElementById('submitButton').addEventListener('click',
+//     function(){
+//         document.querySelector('modal-bg').style.display = 'flex';
+//     });
